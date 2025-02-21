146e1ff71afed382c1a81b007ac7885eb4812b73
taku0403 <taku.pbasket@icloud.com>
2025-02-21 11:48:15 +0900

    Delete nagoyameshi_project directory

--- nagoyameshi_project/nagoyameshi/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.views.generic import TemplateView
-
-class TopView(TemplateView):
-    template_name = "nagoyameshi/top.html"
